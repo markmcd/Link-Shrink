313a7723fa159a9e1c801107b22720ef3d3ad4e8
Mark McDonald <i@ii.net>
2010-05-16 19:43:01 +0800

    Link shrinking now works in independent thread & passes result back to the calling Intent.

--- gen/cd/markm/linkshrink/R.java
+++ gen/cd/markm/linkshrink/R.java
@@ -19,5 +19,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int link_shortened_failure=0x7f040003;
+        public static final int link_shortened_success=0x7f040002;
+        public static final int shortening=0x7f040004;
     }
 }
--- src/cd/markm/linkshrink/Shorten.java
+++ src/cd/markm/linkshrink/Shorten.java
@@ -14,8 +14,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -25,6 +29,8 @@ public class Shorten extends Activity {
 	// TODO add user's override into the config UI
 	private final String BITLY_USERNAME = "linkshrink";
 	private final String BITLY_APIKEY = "R_7c68806f4d99c4751b18ab2aa2261875";
+	
+	private ProgressDialog progress;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,21 +39,57 @@ public class Shorten extends Activity {
 
         if (Intent.ACTION_SEND.equals(intent.getAction())
 				&& "text/plain".equals(intent.getType())) {
+        	
         	String longURL = intent.getStringExtra(Intent.EXTRA_TEXT);
+
         	Log.d(TAG, "Shortening URL: "+longURL);
 
-        	// TODO pull all this out into separate threads & classes
-        	String apiUrl = "http://api.bit.ly/v3/shorten?login="+
-        		BITLY_USERNAME +"&apiKey="+ BITLY_APIKEY +
-        		"&format=json&longUrl="+ URLEncoder.encode(longURL);
+        	progress = ProgressDialog.show(this, getString(R.string.app_name), 
+        			getString(R.string.shortening), true);
+
+        	new RequestThread(this, longURL).start();
         	
-        	HttpClient hc = new DefaultHttpClient();
-        	HttpGet hg = new HttpGet(apiUrl);
-        	try {
+        }
+        else {
+        	Log.e(TAG, "Unknown intent type / data.");
+        }
+ 
+    }
+
+    // thread message handler
+	private Handler handler = new Handler() {
+         @Override
+         public void handleMessage(Message msg) {
+        	 super.handleMessage(msg);
+        	 finish();
+         }
+	};
+	
+	private class RequestThread extends Thread {
+		private Context ctx;
+		private String longURL;
+		
+		public RequestThread(Context ctx, String longURL) {
+			this.ctx = ctx;
+			this.longURL = longURL;
+		}
+		
+		public void run() {
+    		String apiUrl = "http://api.bit.ly/v3/shorten?login="+
+					BITLY_USERNAME +"&apiKey="+ BITLY_APIKEY +
+					"&format=json&longUrl="+ URLEncoder.encode(longURL);
+		
+			HttpClient hc = new DefaultHttpClient();
+			HttpGet hg = new HttpGet(apiUrl);
+			boolean success = false;
+			
+			try {
 				HttpResponse response = hc.execute(hg);
 				Log.i(TAG, "Got API response: "+ response.getStatusLine());
-				
+	
+				String shortURL = longURL;
 				if (response.getStatusLine().getStatusCode() == 200) {
+					
 					// turn the response into a string for the JSON parser
 					InputStreamReader isr = new InputStreamReader(
 							response.getEntity().getContent());
@@ -60,27 +102,32 @@ public class Shorten extends Activity {
 						line = br.readLine();
 					}
 					
+					// parse the JSON response
 					JSONObject json = new JSONObject(sb.toString());
 					if (json.getInt("status_code") == 200) {
 						JSONObject data = json.getJSONObject("data");
 	
-						String shortURL = data.getString("url");
+						shortURL = data.getString("url");
 						Log.i(TAG, "Shortened to: "+shortURL);
-			
-						// TODO report this back to the UI for copy & paste
-						Toast.makeText(getApplicationContext(), shortURL, 
-								Toast.LENGTH_LONG).show();
+						success = true;
 					}
 					else {
 						Log.e(TAG, "JSON returned well-formed but status is "+
 								json.getString("status_txt") +" ("+
 								json.getInt("status_code") +")");
 					}
+					
+					// pass it straight through - this is done here, regardless
+					// of the success of the request so we don't block the use
+					// of a URL when something fails internally - the original
+					// URL still gets passed back
+					Intent passthru = new Intent();
+					passthru.setAction(Intent.ACTION_SEND);
+					passthru.setType("text/plain");
+					passthru.putExtra(Intent.EXTRA_TEXT, shortURL);
+					startActivity(passthru);
 				}
 				else {
-					Toast.makeText(getApplicationContext(), 
-							"Error while contacting API", 
-							Toast.LENGTH_LONG).show();
 					Log.e(TAG, "Status code no good :(");
 				}
 			} catch (ClientProtocolException e) {
@@ -93,11 +140,13 @@ public class Shorten extends Activity {
 				Log.e(TAG, "JSON Exception :(", e);
 				e.printStackTrace();
 			}
-        }
-        else {
-        	Log.e(TAG, "Unknown intent type / data.");
-        }
- 
-    }
+			
+			if (!success)
+				Toast.makeText(ctx, getString(R.string.link_shortened_failure), 
+						Toast.LENGTH_LONG).show();
 
-}
+			handler.sendEmptyMessage(0);
+			progress.dismiss();
+		}
+	}
+ }
